834addd06e11b63399ab18c88e319e63e2b19098
Denisa Roberts <denisa.roberts@denisaroberts.me>
2025-08-05 15:25:33 -0400

    Add the energy for one-layer and the step update

---
+++ src/energies.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import numpy as np
+
+
+def logsumexp(z):
+    """z is a 1D numpy array
+    Computes the log of the sum of exponentials of input elements.
+    Also for stabiltiy, subtracts the maximum value from z before exponentiation.
+    """
+    z_max = np.max(z)
+    return z_max + np.log(np.sum(np.exp(z - z_max)))
+
+
+def softmax(z):
+    """z is a 1D numpy array
+    Computes the softmax of the input elements.
+    """
+    e_z = np.exp(z - np.max(z))  # for numerical stability
+    return e_z / np.sum(e_z)
+
+
+# plan to use the net prediction on the fly
+
+
+def energy(q, X, c_lambda, beta, c_k):
+    """from m.smart paper not the hopfield"""
+    term1 = 0.5 * c_lambda * np.sum(q**2)
+    scores = beta * c_k * (X.T @ q)
+    term2 = (1 / (beta * c_k)) * logsumexp(scores)
+    return term1 - term2
+
+
+def grad_energy(q, X, c_lambda, beta, c_k):
+    """q is the query"""
+    scores = beta * c_k * (X.T @ q)
+    a = softmax(scores)  # so the variant with the softmax
+    return c_lambda * q - X @ a
