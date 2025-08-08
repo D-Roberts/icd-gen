7e67c4c1a0d80b54ed5aec490b87db1b5afc6f50
Denisa Roberts <denisa.roberts@denisaroberts.me>
2025-08-08 11:30:21 -0400

    Add a first patchify functionality for datagen

--- src/new_datagen.py
+++ src/new_datagen.py
@@ -8,6 +8,7 @@ beyond normal.
 """
 import torch
 from torch.distributions import MultivariateNormal
+import torch.nn as nn
 
 
 class DataSampler:
@@ -16,6 +17,17 @@ class DataSampler:
 
     def sample_xs(self):
         raise NotImplementedError
+
+    def patchify(self, im_tensor, patch_size=(32, 32), stride=(32, 32)):
+        """
+        im_tensor: (batch_size, chan, im_dim, im_dim)
+        """
+
+        unfold = nn.Unfold(
+            kernel_size=patch_size, stride=stride
+        )  # not sure if I need padding here or not
+        patches = unfold(im_tensor)
+        return patches
 
 
 def get_data_sampler(data_name, n_dims, **kwargs):
@@ -78,4 +90,32 @@ class GaussianSampler(DataSampler):
 
 # Ad-hoc Test
 im_gen = GaussianSampler(32)
-print(im_gen.sample_xs(b_size=1, seeds=[1]))
+d1 = im_gen.sample_xs(b_size=3, seeds=[0, 1, 2])
+print(d1.shape)
+# reshape im_tensor to (batch_size, channels, height, width)
+d1_reshaped = d1.unsqueeze(1)
+patch_dim = 16
+patches = im_gen.patchify(
+    d1_reshaped, patch_size=(patch_dim, patch_dim), stride=(patch_dim, patch_dim)
+)  # assume square patches non-overlapping from
+# square image
+print(patches.shape)
+# To reshape the patches into a more intuitive format
+#  (e.g., [batch_size, num_patches, channels, patch_height, patch_width]),
+# you can use view and potentially permute:
+
+reshaped_patches = patches.view(
+    d1_reshaped.shape[0],  # batch_size
+    -1,  # Infer the number of patches
+    d1_reshaped.shape[1],  # channels
+    patch_dim,  # patch_height
+    patch_dim,  # patch_width
+)
+
+print(f"Shape after reshaping: {reshaped_patches.shape}")
+
+# I am assuming grey scale and remove the channel dim until needed
+patches = reshaped_patches.squeeze(2)
+
+print(patches.shape)
+# print(patches) # 3 batches with 4 patches each
