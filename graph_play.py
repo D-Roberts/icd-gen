bd9838a7834e26945be100e6adf2d7869c1d64b9
Denisa Roberts <denisa.roberts@denisaroberts.me>
2025-08-08 10:39:25 -0400

    Add a 2-d generator for image like samples

--- graph_play.py
+++ graph_play.py
@@ -37,3 +37,34 @@ print(dataset[0])
 # number
 
 # can't get the dataset though
+
+# ********************Also faiss; likely later not for the step now.**********************
+import faiss
+import numpy as np
+
+# from train_denoiser import args
+
+# POC TODO@DR: alternative to faiss since issue with python3.12
+# for now run with  python3.9 environment
+
+# each row is a patch vector
+image_patches = np.random.rand(32, 256).astype(
+    "float32"
+)  # 10,000 patches of size 16x16 flattened
+
+patch_dim = image_patches.shape[1]
+num_neighbors = 4  # number of nearest neighbors to search for
+
+index = faiss.IndexFlatL2(patch_dim)  # L2 distance index
+index.add(image_patches)  # add patches to the index
+
+query_patch = np.random.rand(1, 256).astype("float32")  # single query patch
+
+# for each pixel, query the index to find its k-nearest neighbors
+# query patch represents the patch centered on the pixel to be denoised
+
+distances, indices = index.search(
+    query_patch, num_neighbors
+)  # search for nearest neighbors
+print("Indices of nearest neighbors:", indices)
+print("Distances to nearest neighbors:", distances)
--- src/new_datagen.py
+++ src/new_datagen.py
@@ -1,29 +1,81 @@
-import faiss
-import numpy as np
+"""
+Patch generation module for new datagen strategies.
 
-# from train_denoiser import args
+@DR: note that there is a torch.distributions.exp_family.ExponentialFamily 
+abstract class to possibly work with in a more flexible/general datagen way
+beyond normal.
 
-# POC TODO@DR: alternative to faiss since issue with python3.12
-# for now run with nlmeans python3.9 environment
+"""
+import torch
+from torch.distributions import MultivariateNormal
 
-# each row is a patch vector
-image_patches = np.random.rand(32, 256).astype(
-    "float32"
-)  # 10,000 patches of size 16x16 flattened
 
-patch_dim = image_patches.shape[1]
-num_neighbors = 4  # number of nearest neighbors to search for
+class DataSampler:
+    def __init__(self):
+        pass
 
-index = faiss.IndexFlatL2(patch_dim)  # L2 distance index
-index.add(image_patches)  # add patches to the index
+    def sample_xs(self):
+        raise NotImplementedError
 
-query_patch = np.random.rand(1, 256).astype("float32")  # single query patch
 
-# for each pixel, query the index to find its k-nearest neighbors
-# query patch represents the patch centered on the pixel to be denoised
+def get_data_sampler(data_name, n_dims, **kwargs):
+    names_to_classes = {
+        "gaussian": GaussianSampler,
+    }
+    if data_name in names_to_classes:
+        sampler_cls = names_to_classes[data_name]
+        return sampler_cls(n_dims, **kwargs)
+    else:
+        print("Unknown sampler")
+        raise NotImplementedError
 
-distances, indices = index.search(
-    query_patch, num_neighbors
-)  # search for nearest neighbors
-print("Indices of nearest neighbors:", indices)
-print("Distances to nearest neighbors:", distances)
+
+class GaussianSampler(DataSampler):
+    """Generate one image-like tensor to patchify later.
+    Args:
+        im_dim - int - image dimension (assuming square images for now)
+        mean - float - mean of the gaussian
+        sigma - float - std.dev of the gaussian
+    Returns:
+        samples - torch.Tensor - sampled images of shape (num_samples, im_dim*im_dim)
+
+
+    """
+
+    def __init__(self, im_dim, sigma=1.0):
+        """for now have an identity covariance scaled by sigma"""
+        super().__init__()
+        self.mean = torch.zeros((im_dim, im_dim))  # assume 0 mean for now
+        # sample uniform later
+        self.sigma = sigma
+        self.im_dim = im_dim
+
+    def sample_xs(self, b_size, seeds=None):
+        """bsize is the batch size here.
+        1 seed for each batch element.
+        """
+        cov_mat = torch.eye(int(self.im_dim)) * self.sigma
+        if seeds is None:
+            # Create the MultivariateNormal distribution
+            mvn = MultivariateNormal(loc=self.mean, covariance_matrix=cov_mat)
+
+            # Sample from the distribution
+            # To get 10 samples, pass a sample_shape of (10,)
+            samples = mvn.sample((b_size,))
+        else:
+            samples = torch.zeros(b_size, self.im_dim, self.im_dim)
+            generator = torch.Generator()
+            assert len(seeds) == b_size
+            for i, seed in enumerate(seeds):
+                # not sure this takes it right
+                generator.manual_seed(seed)
+                # Create the MultivariateNormal distribution
+                mvn = MultivariateNormal(loc=self.mean, covariance_matrix=cov_mat)
+                samples[i] = mvn.sample((1,))
+
+        return samples
+
+
+# Ad-hoc Test
+im_gen = GaussianSampler(32)
+print(im_gen.sample_xs(b_size=1, seeds=[1]))
