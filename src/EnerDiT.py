3d89b5ae1af13871322393dd611a4ae0ae603673
Denisa Roberts <denisa.roberts@denisaroberts.me>
2025-08-19 17:12:17 -0400

    Get started on the EnerDiT

---
+++ src/EnerDiT.py
@@ -0,0 +1,35 @@
+"""
+A modified DiT with DyT for learning energies: the EnerDiT.
+"""
+
+import torch
+import torch.nn as nn
+
+
+# As in Transformers without normalization
+# https://github.com/jiachenzhu/DyT/blob/main/dynamic_tanh.py
+class DyTanh(nn.Module):
+    """"""
+
+    def __init__(self, normalized_shape=16, channels_last=True, alpha_init_value=0.5):
+        super().__init__()
+        self.alpha_init_value = alpha_init_value
+        self.channels_last = channels_last
+
+        self.alpha = nn.Parameter(torch.ones(1) * alpha_init_value)
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+
+    def forward(self, x):
+        x = torch.tanh(self.alpha * x)
+        if self.channels_last:
+            x = x * self.weight + self.bias
+        else:
+            x = x * self.weight[:, None, None] + self.bias[:, None, None]
+        return x
+
+    def extra_repr(self):
+        return f"normalized_shape={self.normalized_shape}, alpha_init_value={self.alpha_init_value}, channels_last={self.channels_last}"
+
+
+# Ad-hoc test
